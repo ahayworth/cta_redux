e4a96dc72bc3b450b9c37d1dcff56f21dcdb0da7
Andrew Hayworth <hayworth@meraki.net>
2015-02-14 20:26:01 -0600

    Manually load dependencies
    Travis seems to load them out of alphabetical order, which I think
    is a known gotcha on the infrastructure.

--- lib/cta_redux.rb
+++ lib/cta_redux.rb
@@ -9,11 +9,21 @@ require "zlib"
 module CTA
   base_path = File.expand_path("..", __FILE__)
   data_dir = File.join(File.expand_path("../..", __FILE__), 'data')
-  Dir.glob("#{base_path}/cta_redux/faraday_middleware/*") { |lib| require lib }
-  Dir.glob("#{base_path}/cta_redux/api/*") { |lib| require lib }
+
+  require "#{base_path}/cta_redux/faraday_middleware/bus_tracker_parser.rb"
+  require "#{base_path}/cta_redux/faraday_middleware/train_tracker_parser.rb"
+  require "#{base_path}/cta_redux/faraday_middleware/customer_alerts_parser.rb"
+  require "#{base_path}/cta_redux/faraday_middleware/simple_cache.rb"
+
+  require "#{base_path}/cta_redux/api/api_response.rb"
+  require "#{base_path}/cta_redux/api/bus_tracker.rb"
+  require "#{base_path}/cta_redux/api/train_tracker.rb"
+  require "#{base_path}/cta_redux/api/customer_alerts.rb"
+
   require "#{base_path}/cta_redux/train_tracker.rb"
   require "#{base_path}/cta_redux/bus_tracker.rb"
   require "#{base_path}/cta_redux/customer_alerts.rb"
+  require "#{base_path}/cta_redux/version.rb"
 
   db_filename = File.join(data_dir, 'cta-gtfs.db')
 
@@ -28,13 +38,18 @@ module CTA
 
   DB = Sequel.sqlite(:database => db_filename, :readonly => true)
 
-  Dir.glob("#{base_path}/cta_redux/models/*") do |lib|
-    next if lib =~ /train/ || lib =~ /bus/
-    require lib
-  end
+  require "#{base_path}/cta_redux/models/agency.rb"
+  require "#{base_path}/cta_redux/models/calendar.rb"
+  require "#{base_path}/cta_redux/models/route.rb"
+  require "#{base_path}/cta_redux/models/shape.rb"
+  require "#{base_path}/cta_redux/models/stop.rb"
+  require "#{base_path}/cta_redux/models/stop_time.rb"
+  require "#{base_path}/cta_redux/models/transfer.rb"
+  require "#{base_path}/cta_redux/models/trip.rb"
 
   require "#{base_path}/cta_redux/models/train.rb"
   require "#{base_path}/cta_redux/models/bus.rb"
+
 end
 
 class Array
