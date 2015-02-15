f3fa0cb840df1f8e74f0ea1d38e446385d72d6c0
Andrew Hayworth <hayworth@meraki.net>
2015-02-14 23:49:40 -0600

    Travis: Don't rely on gem build to unzip DB
    ...of course this would fail on CI.

--- ext/inflate_database/extconf.rb
+++ ext/inflate_database/extconf.rb
@@ -10,6 +10,5 @@ if !File.exists?(db_filename)
   dbf.close
   File.unlink("#{db_filename}.gz")
 end
-puts `ls -lh #{db_filename}`
 
 create_makefile "inflate_database/inflate_database"
--- lib/cta_redux.rb
+++ lib/cta_redux.rb
@@ -26,6 +26,18 @@ module CTA
 
   data_dir = File.join(File.expand_path("../..", __FILE__), 'data')
   db_filename = File.join(data_dir, 'cta-gtfs.db')
+
+  # travis-ci needs to manually unzip things
+  if ENV['TRAVIS'] == 'true'
+    if !File.exists?(db_filename)
+      dbf = File.open(db_filename, 'wb')
+      Zlib::GzipReader.open("#{db_filename}.gz") do |gz|
+        dbf.puts gz.read
+      end
+      dbf.close
+      File.unlink("#{db_filename}.gz")
+    end
+  end
   DB = Sequel.sqlite(:database => db_filename, :readonly => true)
 
   require "#{base_path}/cta_redux/models/agency.rb"
